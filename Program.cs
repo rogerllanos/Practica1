29bc5bb9c50f6beed1727705651b7aad995e222a
rogerllanos <rogerllanos060412@gmail.com>
2025-03-22 16:20:38 -0300

    se crea clases , atributos y metodos

--- Program.cs
+++ Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿    using System;
+
+    namespace practicaClases
+    {
+        public class Celulares 
+        {
+            public string marca;
+            public string modelo;
+            public int almacenamiento;
+            public string ram;
+
+            public void llamar()
+                {
+                    Console.WriteLine("se realiza la llamada");
+                }
+            public void fotografiar()
+                {
+                    Console.WriteLine("se saca una foto con su " + marca);
+                }    
+
+        }
+         public class GrupoFamiliar
+        {
+            public string nombre;
+            public string puestoFamiliar;
+            public int edad;
+            public string sexo;
+
+            public void presentarse()
+                {
+                    Console.WriteLine("me llamos" + nombre + " y soy el " + puestoFamiliar + ",tengo " + edad + " años");
+                }
+            public void trabajar()
+                {
+                    Console.WriteLine("solo los padres trabajan");
+                }    
+
+        }
+    } 
